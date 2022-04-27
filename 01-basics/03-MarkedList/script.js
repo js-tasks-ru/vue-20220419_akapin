b600a9f4e8ebe7a02a298830b22b493e43305571
Alex Kapin <alexander.kapin@outlook.com>
2022-04-27 09:21:21 +0300

    Добавлено решение задачи MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
@@ -29,4 +29,20 @@ const emails = [
   'Isaias_Kuhic@jarrett.net',
 ];
 
-// Требуется создать Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      emails,
+      search: '',
+    };
+  },
+
+  computed: {
+    filteredEmails() {
+      if (!this.search) {
+        return [];
+      }
+      return emails.filter((it) => it.includes(this.search));
+    },
+  },
+}).mount('#app');
