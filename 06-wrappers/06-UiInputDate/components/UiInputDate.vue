cd40bc4ef39a3d7d33825e29c495ccd6834812d4
Александр Капин <aleksandr.kapin@aurus-auto.ru>
2022-05-26 13:28:20 +0300

    Добавлено решение задачи UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <ui-input />
+  <ui-input :model-value="value" :type="type" @input="onInput">
+    <template v-for="slot in Object.keys($slots)" #[slot]>
+      <slot :name="slot" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
@@ -9,5 +13,47 @@ export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (type) => ['date', 'datetime-local', 'time'].includes(type),
+    },
+
+    modelValue: {
+      type: Number,
+      default: null,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    value() {
+      if (typeof this.modelValue === 'undefined' || this.modelValue === null) {
+        return '';
+      }
+      const date = new Date(this.modelValue).toISOString();
+      if (this.type === 'date') {
+        return date.substring(0, 10);
+      } else if (this.type === 'datetime-local') {
+        return date.substring(0, 16);
+      } else if (this.type === 'time') {
+        return this.$attrs['step'] && this.$attrs['step'] % 60 !== 0 ? date.substring(11, 19) : date.substring(11, 16);
+      }
+      return '';
+    },
+  },
+
+  methods: {
+    onInput(event) {
+      if (event.target.value === '') {
+        this.$emit('update:modelValue', null);
+        return;
+      }
+      this.$emit('update:modelValue', event.target.valueAsNumber);
+    },
+  },
 };
 </script>
